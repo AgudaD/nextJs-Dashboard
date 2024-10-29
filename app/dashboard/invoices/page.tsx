30bafc9da69412145e4125402ebbb2ee4a6628e9
AgudaD <agudaoluwanifemi@gmail.com>
2024-10-29 19:59:34 +0100

    fixed path issue in acmelogo.tsx

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,15 @@
+export default function Invoices() {
+  return (
+    <div>
+      <p>Invoices Page</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quis
+        harum suscipit magni maiores numquam unde velit molestias quisquam
+        praesentium, dignissimos minima, repellat eveniet eum omnis illo
+        provident voluptatibus voluptatum excepturi explicabo! Eligendi velit,
+        autem nam earum saepe molestias, minima cupiditate illum sunt sapiente
+        molestiae totam commodi inventore tenetur quod?
+      </p>
+    </div>
+  );
+}
